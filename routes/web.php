d6024e2f2a5d7bcd2ddf0190bcd679e9641fccc6
firdausnanda <firdausnanda46@gmail.com>
2023-04-10 21:30:57 +0700

    ADMIN

--- routes/web.php
+++ routes/web.php
@@ -97,4 +97,73 @@ Route::group(['prefix' => 'superadmin', 'as' => 'superadmin.', 'middleware' => [
 		
 });
 
+// Admin
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth']], function () {
+  
+	// Dashboard
+	Route::get('', [DashboardController::class, 'index'])->name('index');
+
+  // Dosen
+  Route::group(['prefix' => 'dosen', 'as' => 'dosen.'], function () {
+		Route::get('', [DosenController::class, 'index'])->name('index');
+		Route::post('', [DosenController::class, 'store'])->name('store');
+		Route::put('', [DosenController::class, 'update'])->name('update');
+		Route::put('/aktif', [DosenController::class, 'aktif'])->name('aktif');
+	});
+
+  // Matakuliah
+  Route::group(['prefix' => 'matakuliah', 'as' => 'matakuliah.'], function () {
+		Route::get('', [MatakuliahController::class, 'index'])->name('index');
+		Route::post('', [MatakuliahController::class, 'store'])->name('store');
+		Route::put('', [MatakuliahController::class, 'update'])->name('update');
+		Route::delete('', [MatakuliahController::class, 'delete'])->name('delete');
+	});
+
+	// Prodi
+  Route::group(['prefix' => 'prodi', 'as' => 'prodi.'], function () {
+		Route::get('', [ProdiController::class, 'index'])->name('index');
+		Route::post('', [ProdiController::class, 'store'])->name('store');
+		Route::put('', [ProdiController::class, 'update'])->name('update');
+		Route::delete('', [ProdiController::class, 'delete'])->name('delete');
+	});
+	
+	// Tahun Akademik
+  Route::group(['prefix' => 'ta', 'as' => 'ta.'], function () {
+		Route::get('', [TahunAkademikController::class, 'index'])->name('index');
+		Route::post('', [TahunAkademikController::class, 'store'])->name('store');
+		Route::put('', [TahunAkademikController::class, 'update'])->name('update');
+	});
+
+	// Pengajaran
+  Route::group(['prefix' => 'pengajaran', 'as' => 'pengajaran.'], function () {
+		Route::get('', [PengajaranController::class, 'index'])->name('index');
+		Route::post('', [PengajaranController::class, 'store'])->name('store');
+		Route::put('', [PengajaranController::class, 'update'])->name('update');
+		Route::delete('', [PengajaranController::class, 'delete'])->name('delete');
+		Route::get('/kurikulum', [PengajaranController::class, 'kurikulum'])->name('kurikulum');
+		Route::get('/matakuliah', [PengajaranController::class, 'matakuliah'])->name('matakuliah');
+		Route::get('/matakuliah-sks', [PengajaranController::class, 'sks'])->name('sks');
+		Route::post('/sgas', [PengajaranController::class, 'sgas'])->name('sgas');
+		Route::get('/print', [PengajaranController::class, 'print'])->name('print');
+		Route::get('/print-ttd', [PengajaranController::class, 'print_ttd'])->name('print_ttd');
+	});
+
+	// Validasi
+  Route::group(['prefix' => 'validasi', 'as' => 'validasi.'], function () {
+		Route::get('', [ValidasiController::class, 'index'])->name('index');
+		Route::put('', [ValidasiController::class, 'update'])->name('update');
+		Route::put('/bulk-update', [ValidasiController::class, 'update_all'])->name('update_all');
+	});
+
+  // Akun
+  Route::group(['prefix' => 'akun', 'as' => 'akun.'], function () {
+		Route::get('', [AkunController::class, 'index'])->name('index');
+		Route::post('', [AkunController::class, 'store'])->name('store');
+		Route::put('', [AkunController::class, 'update'])->name('update');
+		Route::put('/reset', [AkunController::class, 'reset'])->name('reset');
+		Route::put('/aktif', [AkunController::class, 'aktif'])->name('aktif');
+	});
+		
+});
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
