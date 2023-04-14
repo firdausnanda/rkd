bfe1d9794c282f0f14ad50b0bc24165cce16273e
firdausnanda <firdausnanda@itsk-soepraoen.ac.id>
2023-04-14 10:01:22 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+	return redirect(route('login'));
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 // Superadmin
 Route::group(['prefix' => 'superadmin', 'as' => 'superadmin.', 'middleware' => ['role:superadmin', 'auth']], function () {
