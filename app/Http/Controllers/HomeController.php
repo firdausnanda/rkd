50c28a41b3528e8296f888871515d82efa119901
firdausnanda <firdausnanda@itsk-soepraoen.ac.id>
2023-05-30 13:33:45 +0700

    Fix auth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,28 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        switch (Auth::user()->roles[0]->name) {
+            case 'superadmin':
+                return redirect()->route('superadmin.index');
+                break;
+            case 'admin':
+                return redirect()->route('admin.index');
+                break;
+            case 'prodi':
+                return redirect()->route('prodi.index');
+                break;
+            case 'mwi':
+                return redirect()->route('mwi.index');
+                break;
+            case 'bsdm':
+                return redirect()->route('bsdm.index');
+                break;
+            case 'user':
+                return redirect()->route('user.index');
+                break;
+            default:
+                return redirect()->url('/login');
+                break;
+        }
     }
 }
